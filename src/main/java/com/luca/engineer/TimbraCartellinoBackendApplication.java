d8483366ee3f0bf7f369eba727cf242b56105f26
LucaSoftwareEngineer <santoro.luca.ing@gmail.com>
2026-06-26 19:34:03 +0200

    feat: enable redis repositories

--- src/main/java/com/luca/engineer/TimbraCartellinoBackendApplication.java
+++ src/main/java/com/luca/engineer/TimbraCartellinoBackendApplication.java
@@ -2,8 +2,10 @@ package com.luca.engineer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
+@EnableRedisRepositories
 public class TimbraCartellinoBackendApplication {
 
 	public static void main(String[] args) {

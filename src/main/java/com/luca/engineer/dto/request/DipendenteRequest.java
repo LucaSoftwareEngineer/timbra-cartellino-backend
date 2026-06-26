aee1e71ee029f9273199f5d63dbc0b3d3f68a665
LucaSoftwareEngineer <santoro.luca.ing@gmail.com>
2026-06-26 21:45:43 +0200

    refactor: add validation annotation on DTO DipendenteRequest

--- src/main/java/com/luca/engineer/dto/request/DipendenteRequest.java
+++ src/main/java/com/luca/engineer/dto/request/DipendenteRequest.java
@@ -1,7 +1,10 @@
 package com.luca.engineer.dto.request;
 
+import com.luca.engineer.exceptions.ValidationGroups;
 import com.luca.engineer.models.Ruolo;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,9 +18,32 @@ import lombok.ToString;
 @ToString
 public class DipendenteRequest {
 
+	@NotNull(groups = {ValidationGroups.Update.class}, message = "manca id del dipendente")
+	@NotBlank(groups = {ValidationGroups.Update.class}, message = "manca id del dipendente")
 	private String id;
+	
+	@NotNull(groups = {ValidationGroups.Create.class}, message = "manca nome del dipendente")
+	@NotBlank(groups = {ValidationGroups.Create.class}, message = "manca nome del dipendente")
 	private String nome;
+	
+	@NotNull(groups = {ValidationGroups.Create.class}, message = "manca cognome del dipendente")
+	@NotBlank(groups = {ValidationGroups.Create.class}, message = "manca cognome del dipendente")
 	private String cognome;
+	
+	@NotNull(
+		groups = {
+			ValidationGroups.Create.class,
+			ValidationGroups.Update.class
+		}, 
+		message = "manca ruoloId del dipendente"
+	)
+	@NotBlank(
+		groups = {
+			ValidationGroups.Create.class,
+			ValidationGroups.Update.class
+		}, 
+		message = "manca ruoloId del dipendente"
+	)
 	private String ruoloId;
 	
 }

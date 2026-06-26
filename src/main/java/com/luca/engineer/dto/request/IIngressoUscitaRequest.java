14b157f10ccaff6e9c3084762b5586dad074683d
LucaSoftwareEngineer <santoro.luca.ing@gmail.com>
2026-06-26 23:26:08 +0200

    feat: add DTO for IngressoUscita with validation check

---
+++ src/main/java/com/luca/engineer/dto/request/IIngressoUscitaRequest.java
@@ -0,0 +1,48 @@
+package com.luca.engineer.dto.request;
+
+import java.time.LocalDate;
+
+import com.luca.engineer.exceptions.ValidationGroups;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class IIngressoUscitaRequest {
+
+	@NotNull(groups = {ValidationGroups.Update.class}, message = "id ingresso uscita mancante")
+	@NotBlank(groups = {ValidationGroups.Update.class}, message = "id ingresso uscita mancante")
+	private String id;
+	
+	@NotNull(groups = {ValidationGroups.Create.class}, message = "data ingresso mancante")
+	@NotBlank(groups = {ValidationGroups.Create.class}, message = "data ingresso mancante")
+	private LocalDate dataIngresso;
+	
+	private LocalDate dataUscita;
+	
+	@NotNull(
+		groups = {
+			ValidationGroups.Create.class,
+			ValidationGroups.Update.class
+		}, 
+		message = "id dipendente mancante"
+	)
+	@NotBlank(
+		groups = {
+			ValidationGroups.Create.class,
+			ValidationGroups.Update.class
+		}, 
+		message = "id dipendente mancante"
+	)
+	private String dipendenteId;
+	
+}
---
+++ src/main/java/com/luca/engineer/dto/response/IIngressoUscitaResponse.java
@@ -0,0 +1,25 @@
+package com.luca.engineer.dto.response;
+
+import java.time.LocalDate;
+
+import com.luca.engineer.models.Dipendente;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class IIngressoUscitaResponse {
+
+	private String id;
+	private LocalDate dataIngresso;
+	private LocalDate dataUscita;
+	private Dipendente dipendente;
+	
+}

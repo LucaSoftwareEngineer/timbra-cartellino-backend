a002394be71510a3387786117ce349cf52732e5c
LucaSoftwareEngineer <santoro.luca.ing@gmail.com>
2026-06-26 21:38:08 +0200

    feat: add generic dto for message

---
+++ src/main/java/com/luca/engineer/dto/response/MessageResponse.java
@@ -0,0 +1,20 @@
+package com.luca.engineer.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@ToString
+public class MessageResponse {
+
+	private String msg;
+	
+}

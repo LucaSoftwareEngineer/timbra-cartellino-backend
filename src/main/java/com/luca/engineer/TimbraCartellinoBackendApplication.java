cbb4e5dad78e11b1e11ab0f07380e03586033860
LucaSoftwareEngineer <santoro.luca.ing@gmail.com>
2026-06-26 18:51:15 +0200

    initial commit

---
+++ src/main/java/com/luca/engineer/TimbraCartellinoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.luca.engineer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimbraCartellinoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimbraCartellinoBackendApplication.class, args);
+	}
+
+}
